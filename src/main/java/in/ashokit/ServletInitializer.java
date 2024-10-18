97736f45abd5e8042f46c73a10f6b008a56d0787
mmbabu1988 <85672015+mmbabu1988@users.noreply.github.com>
2024-10-18 09:50:45 +0530

    Update ServletInitializer.java

--- src/main/java/in/ashokit/ServletInitializer.java
+++ src/main/java/in/ashokit/ServletInitializer.java
@@ -1,4 +1,4 @@
-package in.ashokit;
+package in.teksit;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
